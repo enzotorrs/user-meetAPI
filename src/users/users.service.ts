8e833e5ec7df1948e9eff07d12456cb686e78f19
enzotorrs <enzotorres969@gmail.com>
2022-04-19 10:53:51 -0300

    inject repository on userService and add findByUsername method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ){}
+
+    async findByUsername(username: string): Promise<User>{
+        return this.userRepository.findOne({username: username})
+    }
+}

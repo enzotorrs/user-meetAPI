fee52c1852ab46221389945f85060384ea7be31a
enzotorrs <enzotorres969@gmail.com>
2022-04-19 12:00:01 -0300

    add findAllUsers on usersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,9 +8,13 @@ export class UsersService {
     constructor(
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
-    ){}
+    ) { }
 
-    async findByUsername(username: string): Promise<User>{
-        return this.userRepository.findOne({username: username})
+    async findByUsername(username: string): Promise<User> {
+        return await this.userRepository.findOne({ username: username })
+    }
+
+    async findAllUsers(): Promise<User[]> {
+        return await this.userRepository.find()
     }
 }

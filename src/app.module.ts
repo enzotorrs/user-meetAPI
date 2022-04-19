af4c72ddf79c5f152aee6357f3f1598590026ccf
enzotorrs <enzotorres969@gmail.com>
2022-04-19 12:00:53 -0300

    import userModule on appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    AuthModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}

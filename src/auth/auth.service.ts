12a2a1f411f315763e85f72638c6d4438c687d44
enzotorrs <enzotorres969@gmail.com>
2022-04-25 15:06:49 -0300

    create login method in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
     constructor(
         private readonly usersService: UsersService,
+        private readonly jwtService: JwtService,
     ){}
 
     async validateUser(username: string, pass: string){
@@ -14,5 +16,12 @@ export class AuthService {
             return result
         }
         return null;
+    }
+
+    async login(user: any){
+        const payload = {username: user.username, sub: user.userId}
+        return {
+            acess_token: this.jwtService.sign(payload)
+        }
     }
 }

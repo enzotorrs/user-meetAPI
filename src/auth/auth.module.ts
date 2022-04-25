67a0b2d4ffbaef4c6a34d93b3a655ff4f5cdfbe5
enzotorrs <enzotorres969@gmail.com>
2022-04-25 15:07:26 -0300

    configure jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,19 @@ import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
   providers: [AuthService, LocalStrategy],
-  imports: [PassportModule, UsersModule],
+  imports: [
+    PassportModule,
+    UsersModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: {expiresIn: '3600s'}
+    })
+  ],
   controllers: [AuthController]
 })
 export class AuthModule {}

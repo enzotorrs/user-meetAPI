8552611f2607be7adcf8603ce0f1c09900646442
enzotorrs <enzotorres969@gmail.com>
2022-04-25 15:08:04 -0300

    peform login method and return acess token in response

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
 import { LocalGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
+    constructor(
+        private readonly authService: AuthService,
+    ){}
+
     @UseGuards(LocalGuard)
     @Post('login')
     async login(@Request() req){
-        return req.user;
+        return this.authService.login(req.user);
     }
 }

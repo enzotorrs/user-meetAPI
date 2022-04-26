b50ec3c5504a3b0e0f2206b5bd6e6a748eb64fcd
enzotorrs <enzotorres969@gmail.com>
2022-04-26 09:58:09 -0300

    add guard protection for create users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 
@@ -18,6 +19,7 @@ export class UsersController {
         return await this.usersService.findByUsername(username)
     }
 
+    @UseGuards(JwtAuthGuard)
     @Post()
     async saveUser(@Body() user: User): Promise<User> {
         const newUser = await this.usersService.saveUser(user);

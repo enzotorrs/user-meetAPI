356fb4c1934dfb8f5cfe341849c5b79e549b82f5
enzotorrs <enzotorres969@gmail.com>
2022-04-27 10:47:54 -0300

    create meets module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { MeetsModule } from './meets/meets.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     AuthModule,
+    MeetsModule,
   ],
 })
 export class AppModule {}

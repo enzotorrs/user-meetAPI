030683b168c587aec66b092bc7be9d4ff0d5169b
enzotorrs <enzotorres969@gmail.com>
2022-04-27 11:00:05 -0300

    add getAllMeets method in meet service

--- src/meets/meets.service.ts
+++ src/meets/meets.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Meet } from './meet.entity';
 
 @Injectable()
-export class MeetsService {}
+export class MeetsService {
+    constructor(
+        @InjectRepository(Meet)
+        private readonly meetRepository: Repository<Meet>,
+    ){}
+
+    async getAllMeets(): Promise<Meet[]> {
+        return await this.meetRepository.find();
+    }
+}

279860331f782dca922bb2c7f8c84acb70793d94
enzotorrs <enzotorres969@gmail.com>
2022-04-27 10:49:20 -0300

    update meets module

--- src/meets/meets.module.ts
+++ src/meets/meets.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MeetsController } from './meets.controller';
+import { MeetsService } from './meets.service';
 
-@Module({})
+@Module({
+  controllers: [MeetsController],
+  providers: [MeetsService]
+})
 export class MeetsModule {}
